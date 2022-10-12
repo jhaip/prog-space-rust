e7a4da9bfd18cc76130eb09d5ab242cac3afdfef
Jacob Haip <jhaip@mit.edu>
2022-10-12 18:04:47 -0400

    support drawing text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::database::Database;
-use crate::fact::Fact;
+use crate::fact::{Fact, Term};
 
 use std::error::Error;
 
@@ -13,6 +13,8 @@ use opencv::{highgui, prelude::*};
 use std::time::Instant;
 
 use nannou::prelude::*;
+
+use serde_json::{Result, Value};
 
 pub mod database;
 pub mod fact;
@@ -76,12 +78,63 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
 }
 
 fn view(_app: &App, _model: &Model, _frame: Frame) {
-    let draw = _app.draw();
-    draw.background().color(WHITE);
+    let mut db = _model.static_db.lock().unwrap();
+    // let draw = _app.draw();
+    // draw.background().color(WHITE);
+
+    let generic_graphics_wishes = db.select(&vec!["$ wish $ had graphics $graphics".to_string()]);
+    for wish in generic_graphics_wishes.iter() {
+        if let Term::Text(g) = &wish.result.first().unwrap().term {
+            let j: serde_json::Value = serde_json::from_str(&g).unwrap();
+            let draw = nannou::Draw::new();
+            draw.reset();
+            draw.background().color(BLACK);
+            let draw = draw.x_y(-_app.window_rect().w() * 0.5, _app.window_rect().h() * 0.5);
+            // TODO: serialize this into a rust struct so all this unwrap stuff isn't needed
+            for g in j.as_array().unwrap() {
+                let g = g.as_object().unwrap();
+                let typ = g.get("type").unwrap().as_str().unwrap();
+                match typ {
+                    "text" => {
+                        draw.text(
+                            g.get("options")
+                                .unwrap()
+                                .get("text")
+                                .unwrap()
+                                .as_str()
+                                .unwrap(),
+                        )
+                        .right_justify()
+                        // .align_text_top()
+                        .x_y(
+                            g.get("options")
+                                .unwrap()
+                                .get("x")
+                                .unwrap()
+                                .as_f64()
+                                .unwrap()
+                                .to_f32()
+                                .unwrap(),
+                            -g.get("options")
+                                .unwrap()
+                                .get("y")
+                                .unwrap()
+                                .as_f64()
+                                .unwrap()
+                                .to_f32()
+                                .unwrap(),
+                        )
+                        .color(STEELBLUE);
+                    }
+                    _ => println!("unsupported graphics type: {}", typ),
+                }
+            }
+            draw.to_frame(_app, &_frame).unwrap();
+        }
+    }
 
     // TODO: handle slow rx where the video feed produces events faster than we consume them.
     // TODO: use a single_value_channel
-    let mut db = _model.static_db.lock().unwrap();
     if let Ok(seen_programs) = _model.rx.recv() {
         // println!("---{:?}", seen_programs);
         db.retract("#0cv %");
@@ -103,29 +156,32 @@ fn view(_app: &App, _model: &Model, _frame: Frame) {
             ));
         }
 
-        let frame = _model.main_frame.lock().unwrap();
-        if frame.rows() > 0 {
-            // TODO
-            // highgui::imshow("window", &*frame).unwrap();
-            // let buffer = *frame; // TODO: convert
-            if frame.is_continuous() {
-                let data_bytes: &[u8] = frame.data_bytes().unwrap();
-                let size = frame.size().unwrap();
-                let tex = Texture::from_image(
-                    _app,
-                    &nannou::image::DynamicImage::ImageBgr8(nannou::image::ImageBuffer::from_raw(
-                        size.width.to_u32().unwrap(),
-                        size.height.to_u32().unwrap(),
-                        data_bytes.to_vec()
-                    ).unwrap()),
-                );
-                draw.texture(&tex);
-            }
-        } else {
-            println!("waiting for camera to give a frame")
-        }
-        drop(frame);
+        // let frame = _model.main_frame.lock().unwrap();
+        // if frame.rows() > 0 {
+        //     // TODO
+        //     // highgui::imshow("window", &*frame).unwrap();
+        //     // let buffer = *frame; // TODO: convert
+        //     if frame.is_continuous() {
+        //         let data_bytes: &[u8] = frame.data_bytes().unwrap();
+        //         let size = frame.size().unwrap();
+        //         let tex = Texture::from_image(
+        //             _app,
+        //             &nannou::image::DynamicImage::ImageBgr8(
+        //                 nannou::image::ImageBuffer::from_raw(
+        //                     size.width.to_u32().unwrap(),
+        //                     size.height.to_u32().unwrap(),
+        //                     data_bytes.to_vec(),
+        //                 )
+        //                 .unwrap(),
+        //             ),
+        //         );
+        //         draw.texture(&tex);
+        //     }
+        // } else {
+        //     println!("waiting for camera to give a frame")
+        // }
+        // drop(frame);
     }
 
-    draw.to_frame(_app, &_frame).unwrap();
+    // draw.to_frame(_app, &_frame).unwrap();
 }
